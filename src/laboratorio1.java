a756f38512cca10366e526b286834707e5ccfa85
anndre <anndre@anndre-PC>
2017-09-20 16:24:58 -0300

    ola

--- src/laboratorio1.java
+++ src/laboratorio1.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class laboratorio1 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		Random pc =new Random();
+		int usuário1;
+		int usuário2;
+		int computador;
+		
+		System.out.println("usuário1 faça sua jogada");
+		usuário1 = sc.nextInt();
+		
+		System.out.println(" usuário2 faça sua jogada");
+		
+		
+		usuário2 = sc.nextInt();
+		//computador = pc.nextInt(3)+1;
+		
 
+		if(usuário1 == usuário2) {
+		//	if(usuário1 == computador) {
+			System.out.println("enpate");
+		}
+		else if((usuário1- usuário2)== -1 || (usuário1 - usuário2)== 2) {
+			System.out.println("usuário1 vencedor");
+			
+	}
+		else { 
+			System.out.println("usuário2 vencedor");
+		}
+	
+	}
 }
