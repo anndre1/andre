285abfec8d4f7932f8de22cd037817d9cb0920db
anndre <anndre@anndre-PC>
2017-09-21 16:32:53 -0300

    2299

--- src/laboratorio1.java
+++ src/laboratorio1.java
@@ -8,6 +8,7 @@ public class laboratorio1 {
 		int usuário1;
 		int usuário2;
 		int computador;
+		System.out.println("Digite 1 para papel;  2 para pedra ou 3 para tesoura");
 		
 		System.out.println("usuário1 faça sua jogada");
 		usuário1 = sc.nextInt();
@@ -15,21 +16,66 @@ public class laboratorio1 {
 		System.out.println(" usuário2 faça sua jogada");
 		
 		
-		usuário2 = sc.nextInt();
-		//computador = pc.nextInt(3)+1;
+			
 		
+			
+		
+		usuário2 = sc.nextInt();
+		computador = pc.nextInt(3)+1;
+	
+		switch(usuário1) {
+		case 1:
+			System.out.println("usuário1 escolheu papel");
+			break;
+		case 2:
+			System.out.println("usuário1 escolheu pedra");
+			break;
+		case 3 :
+			System.out.println("usuário1 escolheu tesoura");
+			break;
+		}
+		switch(usuário2) {
+		case 1:
+			System.out.println("usuário2 escolheu papel");
+			break;
+		case 2:
+			System.out.println("usuário2 escolheu pedra");
+			break;
+		case 3 :
+			System.out.println("usuário2 escolheu tesoura");
+			break;
+		}
+		/*switch(computador) {
+		case 1:
+			System.out.println("computador escolheu papel");
+			break;
+		case 2:
+			System.out.println("computador escolheu pedra");
+			break;
+		case 3 :
+			System.out.println("computador escolheu tesoura");
+			break;
+		}*/
 
 		if(usuário1 == usuário2) {
 		//	if(usuário1 == computador) {
 			System.out.println("enpate");
 		}
-		else if((usuário1- usuário2)== -1 || (usuário1 - usuário2)== 2) {
-			System.out.println("usuário1 vencedor");
+		else if((usuário1- usuário2)== -1 || (usuário1 - usuário2)== 2) { 
 			
+		
+		//	else if((usuário1- computador)== -1 || (usuário1 - computador)== 2) {
+					System.out.println("usuário1 vencedor");
+					
 	}
 		else { 
 			System.out.println("usuário2 vencedor");
+			//System.out.println("computador vencedor");
 		}
 	
+		}
+	
+
 	}
-}
+
+	
